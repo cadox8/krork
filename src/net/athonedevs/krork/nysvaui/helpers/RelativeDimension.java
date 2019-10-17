8c8382b0ff2d4d24346a4d2e497bb5732ac7ebe6
Cadox8 <cadox8@gmail.com>
2019-10-17 07:02:06 +0200

    Fixed RelativeDimension constructor
    Signed-off-by: Cadox8 <cadox8@gmail.com>

--- src/net/athonedevs/krork/nysvaui/helpers/RelativeDimension.java
+++ src/net/athonedevs/krork/nysvaui/helpers/RelativeDimension.java
@@ -17,7 +17,7 @@ public class RelativeDimension {
 
     private int x = 0, y = 0, width = 0, height = 0;
 
-    RelativeDimension() {}
+    public RelativeDimension() {}
 
     public RelativeDimension(int x, int y, int width, int height) {
         this.x = x;
